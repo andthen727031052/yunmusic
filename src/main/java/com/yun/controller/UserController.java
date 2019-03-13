c0f9d60c81e5bfb3272fe8ff664029b5708da5de
doudoujun <doudoujun945@163.com>
2019-03-13 11:29:09 +0800

    2112

--- src/main/java/com/yun/controller/UserController.java
+++ src/main/java/com/yun/controller/UserController.java
@@ -5,7 +5,10 @@ import com.yun.entity.User;
 import com.yun.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserController {
@@ -14,12 +17,14 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("login.do")
-    public JsonBean login(String uname, String password){
-        return userService.login(uname, password);
+    public JsonBean login(String uname, String password, HttpSession session){
+        return userService.login(uname, password, session);
     }
 
     @PostMapping("adduser.do")
     public JsonBean addUser(User user){
         return userService.addUser(user);
     }
+
+
 }
--- src/main/java/com/yun/service/UserService.java
+++ src/main/java/com/yun/service/UserService.java
@@ -3,11 +3,16 @@ package com.yun.service;
 import com.yun.common.JsonBean;
 import com.yun.entity.User;
 
+import javax.servlet.http.HttpSession;
+
 public interface UserService {
 
     // 登录
-    JsonBean login(String name, String password);
+    JsonBean login(String name, String password, HttpSession session);
 
     // 注册
     JsonBean addUser(User user);
+
+    // 根据ID查询用户数据
+    JsonBean findUserInfoByUid(Integer uid);
 }
